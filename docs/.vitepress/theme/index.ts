25476d45604f8a05f983fa9795c5383c01190870
tohru <65994850+Tohrusky@users.noreply.github.com>
2023-07-15 02:26:43 +0800

    fix import error，全局引入naive-ui一把梭

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,28 +1,16 @@
 import Theme from 'vitepress/theme'
 import './style/var.css'
-import {
-  // create naive ui
-  create,
-  // component
-  NButton,
-  NIcon,
-  NSpace,
-  NGradientText
-} from 'naive-ui'
+import { install } from 'naive-ui'
 
 import TranslationButton from '../../components/TranslationButton.vue'
 import DarkModeButton from '../../components/DarkModeButton.vue'
 import NavigationButton from '../../components/NavigationButton.vue'
 import ExternalLinkButton from '../../components/ExternalLinkButton.vue'
 
-const naive = create({
-  components: [NButton, NIcon, NSpace, NGradientText]
-})
-
 export default {
   ...Theme,
   enhanceApp({ app }): void {
-    app.use(naive)
+    install(app);
 
     app.component('TranslationButton', TranslationButton)
     app.component('DarkModeButton', DarkModeButton)
